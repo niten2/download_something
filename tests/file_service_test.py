f48e0fc34d8537661c19aeb7c9566e73a871a41d
niten2 <niten2@yandex.ru>
2019-12-14 22:05:46 +0700

    add new tests

--- tests/file_service_test.py
+++ tests/file_service_test.py
@@ -1,7 +1,10 @@
 from src.file_service import file_service
 
-
 class TestFileService:
-	def test_remove_link(self):
-		file_service.remove_link('tttttt')
-		assert 1 == 1
+    # def test_remove_link(self):
+    # 	file_service.remove_link('tttttt')
+    # 	assert 1 == 1
+
+    def test_append_lines(self):
+        file_service.append_lines('tttttt, dir')
+        assert 1 == 1
--- tests/fixtures/links.py
+++ tests/fixtures/links.py
@@ -1,3 +1,5 @@
 git_url = 'https://github.com/niten2/test_tasks'
 youtube_video_url = "https://www.youtube.com/watch?v=jcAX1RtKq3U"
+youtube_mp3_url = "https://www.youtube.com/watch?v=jcAX1RtKq3U"
 file_url = "https://cdn.sstatic.net/Sites/stackoverflow/img/apple-touch-icon@2.png"
+youtube_playlist_url = "https://www.youtube.com/watch?v=J88vmR6D3dY&list=PL5bLw9Uguvv3p9MPHUvI0WDEeRhrlgR7x"
