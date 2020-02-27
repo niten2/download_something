3862d60e6cc4c19690e0cc757b2de4b88b85cc59
niten2 <niten2@yandex.ru>
2020-02-27 13:44:11 +0700

    fix import in youtube mp3

--- src/downloaders/youtube_mp3.py
+++ src/downloaders/youtube_mp3.py
@@ -1,18 +1,22 @@
 from __future__ import unicode_literals
 import youtube_dl
 from src.settings import MP3_PATH
+from src.file_service import file_service
+
 
 class YoutubeMp3:
     def is_type(self, url, flag):
         return 'youtube' in url and flag == 'mp3'
 
-    def download(self, url, dir):
+    def download(self, url, dir='other'):
         full_path = MP3_PATH + dir + '/'
         file_service.create_dir(full_path)
 
         ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': full_path + '%(title)s.%(ext)s',
+            'format':
+            'bestaudio/best',
+            'outtmpl':
+            full_path + '%(title)s.%(ext)s',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -22,5 +26,6 @@ class YoutubeMp3:
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
+
 
 youtube_mp3 = YoutubeMp3()
