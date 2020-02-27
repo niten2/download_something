1e78b6ba5bba2be96adaf22562a60a11b570657c
niten2 <niten2@yandex.ru>
2020-02-27 13:45:27 +0700

    add subtitles in youtube video

--- src/downloaders/youtube_video.py
+++ src/downloaders/youtube_video.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
-import youtube_dl
-from src.utils import utils
-from src.settings import VIDEO_PATH
 from src.file_service import file_service
+from src.settings import VIDEO_PATH
+import youtube_dl
+
 
 class YoutubeVideo:
     def is_type(self, url):
@@ -13,11 +13,15 @@ class YoutubeVideo:
         file_service.create_dir(full_path)
 
         ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'format':
+            'bestvideo[height<=?720]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'writesubtitles': 'write-sub=',
+            'subtitleslangs': ['ru', 'en'],
             'outtmpl': full_path + '%(title)s.%(ext)s',
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
+
 
 youtube_video = YoutubeVideo()
