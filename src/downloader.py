77a0a64085658e527ab9f9e63e963ebb0e182c15
niten2 <niten2@yandex.ru>
2020-02-27 13:43:31 +0700

    modified src/downloader.py

--- src/downloader.py
+++ src/downloader.py
@@ -6,8 +6,18 @@ from src.downloaders.youtube_mp3 import youtube_mp3
 from src.downloaders.youtube_video import youtube_video
 from src.downloaders.youtube_playlist import youtube_playlist
 
-def get_log_string(prefix, url, dir=' ', flag=' '):
-    return prefix + ' ' + url + ', ' + dir + ', ' + flag
+errors = [
+    "Name or service not known",
+    "Too Many Requests",
+    "url_encoded_fmt_stream_map",
+    "Temporary failure in name resolution",
+    "Connection reset by peer",
+]
+
+
+def get_log_string(url, dir=' ', flag=' ', prefix=''):
+    return (url + ', ' + dir + ', ' + flag + ', ' + prefix)
+
 
 class Downloader:
     def process(self):
@@ -20,9 +30,11 @@ class Downloader:
         dir = link['dir']
         flag = link['flag']
 
-        print('process', url, dir, flag)
+        print('processing', url, dir, flag)
 
         try:
+            raise Exception("Name or service not known")
+
             if youtube_playlist.is_type(url, flag):
                 logger.info('ACTION youtube_playlist ' + url)
                 urls = youtube_playlist.process(url, dir)
@@ -31,35 +43,30 @@ class Downloader:
             elif git.is_type(url):
                 logger.info('ACTION git ' + url)
                 git.download(url)
+            elif youtube_mp3.is_type(url, flag):
+                logger.info('ACTION youtube video mp3 ' + url)
+                youtube_mp3.download(url, dir)
             elif youtube_video.is_type(url):
                 logger.info('ACTION youtube video ' + url)
                 youtube_video.download(url, dir)
-            elif youtube_mp3.is_type(url, flag):
-                logger.info('ACTION youtube video mp3 ' + url)
-                youtube_mp3.download(url)
             else:
                 logger.info('ACTION wget download ' + url)
                 wget.download(url)
 
             logger.warning(get_log_string('DOWNLOAD LINK:', url, dir, flag))
             file_service.remove_link(url)
-        except Exception as e:
-            # TODO refactoring
-            if "Too Many Requests" in str(e):
-                logger.warning(get_log_string(str(e) + ' :', url, dir, flag))
-                return False
+        except Exception as err:
+            for name in errors:
+                if name in str(err):
+                    logger.warning(get_log_string(url, dir, flag, str(err)))
+                    return False
 
-            if "url_encoded_fmt_stream_map" in str(e):
-                logger.warning(get_log_string(str(e) + ' :', url, dir, flag))
-                return False
+            logger.warning(
+                get_log_string(url, dir, flag, 'REMOVE LINK ' + str(err)))
 
-            if "Temporary failure in name resolution" in str(e):
-                logger.warning(get_log_string(str(e) + ' :', url, dir, flag))
-                return False
-
-            logger.warning(get_log_string('REMOVE LINK ' + str(e) + ' :', url, dir, flag))
             file_service.remove_link(url)
 
         return True
+
 
 downloader = Downloader()
